c63bdd0a8722ad059c88cf2f21df9f766aa6b965
arnaumontsia <arnaucaballer@iesmontisa.org>
2026-05-05 08:40:06 +0200

    Canvis a coses innecesaries a arxius

--- models/user.py
+++ models/user.py
@@ -1,12 +1,8 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 
 class User:
-    def __init__(self, username, password, total_score=0, is_hashed=False, anotacions="", vist=False):
+    def __init__(self, username, password, is_hashed=False):
         self.username = username
-        self.total_score = total_score
-        
-        self.__anotacions = anotacions
-        self.vist = vist
         
         if is_hashed:
             self.password = password
@@ -16,31 +12,11 @@ class User:
     def check_password(self, password_attempt):
         return check_password_hash(self.password, password_attempt)
 
-    def add_score(self, points):
-        self.total_score += points
-
-    def get_anotacions(self):
-        return self.__anotacions
-
-    def set_anotacions(self, text):
-        self.__anotacions = text
-
     def to_dict(self):
         return {
             "username": self.username,
-            "password": self.password,
-            "total_score": self.total_score,
-            "anotacions": self.get_anotacions(),
-            "vist": self.vist
+            "password": self.password
         }
 
     def __str__(self):
         return f"User: {self.username} | Total Score: {self.total_score}"
-
-class Alumne(User):
-    def __init__(self, username, password, total_score=0, is_hashed=False, anotacions="", vist=False):
-        super().__init__(username, password, total_score, is_hashed, anotacions, vist)
-
-class Professor(User):
-    def __init__(self, username, password, total_score=0, is_hashed=False, anotacions="", vist=False):
-        super().__init__(username, password, total_score, is_hashed, anotacions, vist)
